e94a59cc2855b1f7042bab6976834b28d53983b1
JaryMontoya <jarycarolinam125@gmail.com>
2024-03-28 02:33:17 -0500

    commit

--- src/main/java/co/com/ps/javaii/service/TipoDocumentoService.java
+++ src/main/java/co/com/ps/javaii/service/TipoDocumentoService.java
@@ -1,21 +1,31 @@
 package co.com.ps.javaii.service;
 
 import co.com.ps.javaii.model.TipoDocumento;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class TipoDocumentoService implements ITipoDocumentoService {
 
+    private final TipoDocumentoRepository tipoDocumentoRepository;
+
     @Override
-    public List<TipoDocumento> getTipoDocumento(); }
-        return null;
-}
-@Override
-public void DeleteTipoDocumento();
-}
+    public List<TipoDocumento> getTipoDocumento() {
+        return tipoDocumentoRepository;
+    }
 
 
-}
+   }
+   @Override
+  public void deleteTipoDocumento();
+
+
+
+
+
 
 
 
